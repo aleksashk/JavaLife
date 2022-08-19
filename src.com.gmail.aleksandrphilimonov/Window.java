1f59f0021ed28a8354ce9a99438c961aa0ad6405
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-08-19 15:25:35 +0300

    применили константы из класса Config в классе Window

--- src.com.gmail.aleksandrphilimonov/Window.java
+++ src.com.gmail.aleksandrphilimonov/Window.java
@@ -11,7 +11,7 @@ public class Window implements Runnable {
     void initFrame() {
         frame = new JFrame();
         frame.getContentPane().setLayout(null);
-        frame.setSize(300, 200);
+        frame.setSize(Config.SIZE * Config.WIDTH, Config.SIZE * Config.HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);//размещение окна по центру экрана
         frame.setVisible(true);
