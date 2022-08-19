23bd97259efff787a66af4d737c3d75a568ff482
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-08-19 15:20:46 +0300

    стартовые настройки JFrame

--- src.com.gmail.aleksandrphilimonov/Window.java
+++ src.com.gmail.aleksandrphilimonov/Window.java
@@ -3,9 +3,18 @@ import javax.swing.*;
 public class Window implements Runnable {
     JFrame frame;
 
-
     @Override
     public void run() {
+        initFrame();
+    }
 
+    void initFrame() {
+        frame = new JFrame();
+        frame.getContentPane().setLayout(null);
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);//размещение окна по центру экрана
+        frame.setVisible(true);
+        frame.setTitle("Life Game");
     }
 }
