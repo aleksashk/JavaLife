94e01b46bc51c5f4ee81601d0c0835d7efdeec31
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-08-19 16:19:29 +0300

    finish project

--- src.com.gmail.aleksandrphilimonov/Config.java
+++ src.com.gmail.aleksandrphilimonov/Config.java
@@ -1,10 +1,10 @@
 import java.awt.*;
 
 public class Config {
-    public static final int SIZE = 20;// размер одной ячейки
-    public static final int WIDTH = 40;// ширина поля
-    public static final int HEIGHT = 30;// высота поля
-    public static final int SLEEPMS = 500;
+    public static final int SIZE = 10;// размер одной ячейки
+    public static final int WIDTH = 80;// ширина поля
+    public static final int HEIGHT = 60;// высота поля
+    public static final int SLEEPMS = 100;
 
     //метод определяет цвета
     public static Color getColor(Status status) {
