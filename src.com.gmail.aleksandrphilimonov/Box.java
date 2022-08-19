6abc14b318899b7e322a9d3148c782d37c298e49
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-08-19 15:37:28 +0300

    создали boxes и заполнили матрицу этими boxes

--- src.com.gmail.aleksandrphilimonov/Box.java
+++ src.com.gmail.aleksandrphilimonov/Box.java
@@ -4,7 +4,9 @@ import java.awt.*;
 public class Box extends JPanel {
     public Box(int x, int y) {
         super();
-        setBounds(x * Config.SIZE, y * Config.SIZE, Config.SIZE, Config.SIZE);//задаём координаты и размеры панели
+
+        //задаём координаты и размеры панели
+        setBounds(x * Config.SIZE, y * Config.SIZE, Config.SIZE, Config.SIZE);
         setBackground(Color.BLUE);
     }
 }
--- src.com.gmail.aleksandrphilimonov/Window.java
+++ src.com.gmail.aleksandrphilimonov/Window.java
@@ -2,19 +2,31 @@ import javax.swing.*;
 
 public class Window implements Runnable {
     JFrame frame;
+    Box[][] boxes;//матрица для хранения организмов
 
     @Override
     public void run() {
         initFrame();
+        initBoxes();
     }
 
     void initFrame() {
         frame = new JFrame();
         frame.getContentPane().setLayout(null);
-        frame.setSize(Config.SIZE * Config.WIDTH, Config.SIZE * Config.HEIGHT);
+        frame.setSize(Config.SIZE * Config.WIDTH + 25, Config.SIZE * Config.HEIGHT + 45);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);//размещение окна по центру экрана
         frame.setVisible(true);
         frame.setTitle("Life Game");
+    }
+
+    void initBoxes() {
+        boxes = new Box[Config.WIDTH][Config.HEIGHT];
+        for (int x = 0; x < Config.WIDTH; x++) {
+            for (int y = 0; y < Config.HEIGHT; y++) {
+                boxes[x][y] = new Box(x, y);
+                frame.add(boxes[x][y]);
+            }
+        }
     }
 }
