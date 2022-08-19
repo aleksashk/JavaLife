272e77902f1dc9568147d1c9326cda3742008342
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-08-19 15:27:41 +0300

    создали класс Box - это панель

--- src.com.gmail.aleksandrphilimonov/Config.java
+++ src.com.gmail.aleksandrphilimonov/Config.java
@@ -1,6 +1,6 @@
 public class Config {
     public static final int SIZE = 20;// размер одной ячейки
     public static final int WIDTH = 40;// ширина поля
-    public static final int HEIGHT = 60;// высота поля
+    public static final int HEIGHT = 30;// высота поля
 
 }
