dc37cb362c4242771762eb79504c6db3b918f0f4
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-08-19 15:30:48 +0300

    описали панель Box, это ячейка, которая представляет один организм в игре

--- src.com.gmail.aleksandrphilimonov/Box.java
+++ src.com.gmail.aleksandrphilimonov/Box.java
@@ -1,3 +1,10 @@
-public class Box {
+import javax.swing.*;
+import java.awt.*;
 
+public class Box extends JPanel {
+    public Box(int x, int y) {
+        super();
+        setBounds(x * Config.SIZE, y * Config.SIZE, Config.SIZE, Config.SIZE);//задаём координаты и размеры панели
+        setBackground(Color.BLUE);
+    }
 }
